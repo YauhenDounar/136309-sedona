5290e8b066fe5427acc59d432dbfa8a6bcb025fe
YauhenDounar <YauhenDounar@gmail.com>
2016-02-15 22:05:20 +0800

    на итоговоу проверку

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 /**
  * Created by yauhen on 14.02.2016.
  */
-var link = document.querySelector(".form-open-btn");
+var button = document.querySelector(".form-open-btn");
 var popup = document.querySelector(".hotels-form");
 var form = popup.querySelector("form");
 var dateIn =popup.querySelector("[name=date-in]");
@@ -9,36 +9,47 @@ var dateOut =popup.querySelector("[name=date-out]");
 var map = document.querySelector(".map");
 var mapInteractive = document.querySelector(".modal-content-map");
 
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.toggle("hotels-form-show");
+button.addEventListener("click", function(event) {
+  if (popup.classList.contains("hotels-form-show") || popup.classList.contains("hotels-form-error")) {
+    event.preventDefault();
+    popup.classList.remove("hotels-form-show");
+    popup.classList.remove("hotels-form-error");
+  } else {
+    event.preventDefault();
+    popup.classList.add("hotels-form-show");
+  }
 });
 
 form.addEventListener("submit", function(event) {
   if (!dateIn.value || !dateOut.value) {
     event.preventDefault();
-//        popup.classList.add("hotels-form-error");
+        popup.classList.remove("hotels-form-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("hotels-form-error");
+  } else {
+    //event.preventDefault();
+    popup.classList.remove("modal-content-show");
   }
 });
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("hotels-form-show")) {
-      popup.classList.remove("hotels-form-show");
-//          popup.classList.remove("hotels-form-error");
-    }
-  }
-});
-
-map.addEventListener("click", function(event) {
-  event.preventDefault();
-  mapInteractive.classList.add("modal-content-show");
-});
-
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (mapInteractive.classList.contains("modal-content-show")) {
-      mapInteractive.classList.remove("modal-content-show");
-    }
-  }
-});
+//
+//window.addEventListener("keydown", function(event) {
+//  if (event.keyCode === 27) {
+//    if (popup.classList.contains("hotels-form-show")) {
+//      popup.classList.remove("hotels-form-show");
+//      popup.classList.remove("hotels-form-error");
+//    }
+//  }
+//});
+//
+//map.addEventListener("click", function(event) {
+//  event.preventDefault();
+//  mapInteractive.classList.add("modal-content-show");
+//});
+//
+//window.addEventListener("keydown", function(event) {
+//  if (event.keyCode === 27) {
+//    if (mapInteractive.classList.contains("modal-content-show")) {
+//      mapInteractive.classList.remove("modal-content-show");
+//    }
+//  }
+//});
